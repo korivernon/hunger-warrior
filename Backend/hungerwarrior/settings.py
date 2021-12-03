e422a3c1a9a0680e1e2ef2710d21884c11a56c05
Kori Vernon <trapbookpro@10-18-46-102.dynapool.wireless.nyu.edu>
2021-12-03 10:03:55 -0500

    adding allowed origins

--- Backend/hungerwarrior/settings.py
+++ Backend/hungerwarrior/settings.py
@@ -58,7 +58,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:3002"
+    "http://localhost:3002", 
+    "http://hungerwarriorapi.herokuapp.com",
+    "https://hungerwarriorapi.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'hungerwarrior.urls'
